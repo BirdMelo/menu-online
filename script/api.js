b1436972ff1ea269e6efdad9e21be6f029b2f6a1
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2025-03-31 19:25:42 -0300

    implementando database externo com github gist

--- script/api.js
+++ script/api.js
@@ -1,10 +1,10 @@
-const DATA_BASE = 'http://localhost:3001/'
+const DATA_BASE = 'https://gist.githubusercontent.com/BirdMelo/90d566ec479b51109d53dcd88f6f6496/raw/922bd054fad7ac94cee18c8986e4af1c2f6ebe90/dbMenu'
 
 const api = {
     async getMenu() {
         try {
-            const menu = await axios.get(`${DATA_BASE}menu`)
-            return menu.data
+            const menu = await axios.get(`${DATA_BASE}`)
+            return menu.data.menu
 
         } catch (error) {
             alert("Erro ao buscar menu")
